45316e62ea803810d9dcd708a6ab24b46f8a627b
beingajaat <malikakshay0420@gmail.com>
2023-04-16 22:11:20 +0530

    a

--- userdata/models.py
+++ userdata/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class userData(models.Model):
-    name = models.CharField(max_length=50 (attrs=={'required': 'True'}))
-    email = models.CharField(max_length=50 (attrs=={'required': 'True'}))
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50 )
     password = models.CharField(max_length=50)
 
 # Create your models here.
